2101cc0525acce47345edd163236aecf7123f53e
Wailan Tirajoh <wailantirajoh@gmail.com>
2025-06-21 17:32:40 +0700

    refactor(controller): remove unused code from EntityRelationshipDiagramsController#index

--- app/controllers/dbviewer/entity_relationship_diagrams_controller.rb
+++ app/controllers/dbviewer/entity_relationship_diagrams_controller.rb
@@ -1,21 +1,6 @@
 module Dbviewer
   class EntityRelationshipDiagramsController < ApplicationController
     def index
-      # Only show warning if no tables exist, but don't fetch relationships on initial load
-      if @tables.blank?
-        flash.now[:warning] = "No tables found in database to generate ERD."
-      end
-
-      respond_to do |format|
-        format.html # Just render the HTML without relationships
-        format.json do
-          # For JSON requests, return just tables initially
-          render json: {
-            tables: @tables,
-            relationships: []
-          }
-        end
-      end
     end
   end
 end
